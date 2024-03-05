c64c3e7be904ccc1a4c857cf74ba0952d9ed0012
nguyenngochuy00 <ngochuy25120@gmail.com>
2024-03-05 22:36:23 +0700

    Add features/subdivision

--- wp-content/themes/vinhome-vuyen/templates/subdivision.php
+++ wp-content/themes/vinhome-vuyen/templates/subdivision.php
@@ -36,7 +36,7 @@ $post_type = get_field('post_type_slug');
     <div class="lav-page-body">
         <div class="container">
             <!-- Items list -->
-            <div class="row">
+             <div class="row row-cols-1">
                 <?php
                 $posts = get_posts(array(
                     'post_type'         => $post_type,
@@ -46,28 +46,30 @@ $post_type = get_field('post_type_slug');
                     'order'             => 'ASC'
                 ));
                 if ($posts) : ?>
-                    <div>
                         <?php foreach ($posts as $post) :
                             setup_postdata($post)
                         ?>
                             <!-- TODO -->
-                            <div class="lav-subdivision-item">
-                                <!-- Image -->
-                                <?php
-                                $image_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->id), 'full')[0];
-                                ?>
-                                <img height="200" src="<?php echo $image_url; ?>" alt="" />
+                            <div class="col col-xl-6 col-sm-12">
+                                <div class="lav-subdivision-item">
+                                    <!-- Image -->
+                                    <?php
+                                    $image_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->id), 'full')[0];
+                                    ?>
+                                    <img src="<?php echo $image_url; ?>" alt="" />
+                                    
 
-                                <!-- Link | Title | Date -->
-                                <a href="<?php the_permalink($post->id); ?>">
-                                    <?php echo get_the_title($post->id); ?> - <span><?php echo get_the_date($post->id); ?></span>
-                                </a>
-
-                                <!-- Content -->
-                                <?php the_content($post->id); ?>
+                                    <!-- Link | Title | Date -->
+                                    <a href="<?php the_permalink($post->id); ?>">
+                                        <?php echo get_the_title($post->id); ?> 
+                                        <!-- - <span><?php echo get_the_date($post->id); ?></span> -->
+                                    </a>
+                                    
+                                    <!-- Content -->
+                                    <?php the_content($post->id); ?>
+                                </div>
                             </div>
                         <?php endforeach; ?>
-                    </div>
                     <?php wp_reset_postdata(); ?>
                 <?php endif; ?>
             </div>
