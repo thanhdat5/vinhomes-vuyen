5cbe1ecfd3e96595c8b92b9cc9b2bf9f7e0a015b
Dacdd <im3d.11399@gmail.com>
2024-03-06 22:39:01 +0700

    refacter code contact form ui

--- wp-content/themes/vinhome-vuyen/templates/contact.php
+++ wp-content/themes/vinhome-vuyen/templates/contact.php
@@ -59,9 +59,9 @@ $map_embeded = get_theme_mod('lav_map', '');
                                 <path d="M3.33366 3.33331H16.667C17.5837 3.33331 18.3337 4.08331 18.3337 4.99998V15C18.3337 15.9166 17.5837 16.6666 16.667 16.6666H3.33366C2.41699 16.6666 1.66699 15.9166 1.66699 15V4.99998C1.66699 4.08331 2.41699 3.33331 3.33366 3.33331Z" stroke="#222222" stroke-linecap="round" stroke-linejoin="round" />
                                 <path d="M18.3337 5L10.0003 10.8333L1.66699 5" stroke="#222222" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
-                            <span >
-                                <a  href="mailto:<?php echo $email; ?>"><?php echo $email; ?>
-                            </span>
+                            
+                            <a  href="mailto:<?php echo $email; ?>"><?php echo $email; ?>
+                            
                         </div>
                     <?php
                     } ?>
